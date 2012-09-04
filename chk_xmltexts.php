daf6f814d0017aace68fc42022f179f52ccb4b55
Satoru Yoshida <ramat@ram.ne.jp>
2012-09-04 23:49:56 +0900

    code cleaning

--- chk_xmltexts.php
+++ chk_xmltexts.php
@@ -1,16 +1,22 @@
 <?php
-//list up files
 require_once 'chk_var.php';
+
 $basePath = $target_path;
 $files = scan_file($basePath);
 
 foreach ($files as $file) {
-	$a = parseFile($file);
+	$labels = parseFile($file);
+	if ($labels === false) {
+		printFileAttribute($file, 'unsupported');
+	} else {
+		printLabels($labels);
+	}
+	echo '<hr>', "\n";
 }
 
 /**
- * @param string $dir
- * @return array
+ * @param string $dir target path
+ * @return array xml files
  */
 function scan_file($dir) {
 	$dir = rtrim($dir, '/') . '/';
@@ -29,159 +35,186 @@ function scan_file($dir) {
 }
 
 /**
- * @param array $param
+ * @param string $file
+ * @return false|array labels if supported, false if not.
  */
-function sortAndList($param) {
-	sort($param, SORT_STRING);
-	foreach ($param as $value) {
-		echo $value, '<BR>',"\n";
+function parseFile($file){
+	$contents = file_get_contents($file);
+	$xml = simplexml_load_string( $contents , 'SimpleXMLElement', LIBXML_NOBLANKS );
+	$labels = array();
+
+	//judges as unsupported file by root element
+	$isPlugin = $xml->xpath('/plugin');
+	if(count($isPlugin) > 0){
+		return false;
 	}
-	echo '<hr>',"\n";
-	return;
+
+	$isProject = $xml->xpath('/project');
+	if(count($isProject) > 0){
+		return false;
+	}
+
+	//judges as Table of Contents if root element is toc
+	$isToc = $xml->xpath('/toc');
+	if(count($isToc) > 0){
+		printFileAttribute($file, 'Table of Contents');
+		//toc title
+		$labels[] = $isToc['0']['label'];
+		//various levels exist for topic
+		foreach ($xml->xpath('//topic') as $topic){
+			$labels[] = $topic['label'];
+		}
+		return $labels;
+	}
+
+	//judges as Context Help if contexts element is in root
+	$isContextHelp = $xml->xpath('/contexts');
+	if(count($isContextHelp) > 0){
+		printFileAttribute($file, 'Context Help');
+
+		printTableHeader('id', 'desc.');
+		foreach ($xml->xpath('/contexts/context') as $context){
+			printTableRow($context['id'], $context->description);
+		}
+		printTableEnd();
+
+		foreach ($xml->xpath('/contexts/context/topic') as $topic){
+			$labels[] = $topic['label'];
+		}
+		return $labels;
+	}
+
+	//judges as Simple Cheat Sheet if cheatsheet element is in root
+	$isCheatsheet = $xml->xpath('/cheatsheet');
+	if(count($isCheatsheet) > 0){
+		printFileAttribute($file, 'Cheat Sheet');
+
+		printTableHeader('title', 'intro desc.');
+		foreach ($xml->xpath('/cheatsheet') as $cheatsheet){
+			$desc = (string)$cheatsheet->intro->description->b;
+			if (strlen($desc) == 0 ) {
+				$desc = (string)$cheatsheet->intro->description;
+			}
+			printTableRow($cheatsheet['title'], $desc);
+		}
+		printTableEnd();
+
+		printTableHeader('title', 'desc.');
+		foreach ($xml->xpath('/cheatsheet/item') as $item){
+			$desc = $item->description;
+			printTableRow($item['title'], $desc);
+		}
+		printTableEnd();
+
+		return $labels;
+	}
+
+	//judges as Composite Cheat Sheet if compositecheatsheet element is in root
+	$isComposite = $xml->xpath('/compositeCheatsheet');
+	if(count($isComposite) > 0){
+		printFileAttribute($file, 'Composite Cheat Sheet');
+		//sheet name
+		$labels[] = $isComposite['0']['name'];
+
+		printTableHeader('group name', 'intro', 'compl.');
+		foreach ($xml->xpath('/compositeCheatsheet/taskGroup') as $taskGroup){
+			$intro = (string)$taskGroup->intro->b;
+			if (strlen($intro) == 0 ) {
+				$intro = (string)$taskGroup->intro;
+			}
+			$compl = (string)$taskGroup->onCompletion->b;
+			if (strlen($compl) == 0 ) {
+				$compl = (string)$taskGroup->onCompletion;
+			}
+			printTableRow($taskGroup['name'], $intro, $compl);
+		}
+		printTableEnd();
+
+		printTableHeader('name', 'intro', 'compl.');
+		foreach ($xml->xpath('/compositeCheatsheet/taskGroup/task') as $task){
+			$intro = (string)$task->intro->b;
+			if (strlen($intro) == 0 ) {
+				$intro = (string)$task->intro;
+			}
+			$compl = (string)$task->onCompletion->b;
+			if (strlen($compl) == 0 ) {
+				$compl = (string)$task->onCompletion;
+			}
+			printTableRow($task['name'], $intro, $compl);
+		}
+		printTableEnd();
+
+		return $labels;
+	}
+
+	return false;
 }
 
 /**
  * @param string $file
- * @return boolean|string Type
+ * @param string $type
+ * @return void
  */
-function parseFile($file){
-$contents = file_get_contents($file);
-$xml = simplexml_load_string( $contents , 'SimpleXMLElement', LIBXML_NOBLANKS );
-
-//judges as unsupported file by root element
-$isPlugin = $xml->xpath('/plugin');
-if(count($isPlugin) > 0){
-	echo 'file name: ',"\n", $file,"\n", ' is unsupported.<BR>',"\n";
-	return false;
+function printFileAttribute($file, $type) {
+	echo 'file name: ', $file, '<BR>', "\n",
+		 'file type: ', $type, '<BR>', "\n";
 }
 
-$isProject = $xml->xpath('/project');
-if(count($isProject) > 0){
-	echo 'file name: ',"\n", $file,"\n", ' is unsupported.<BR>',"\n";
-	return false;
-}
-
-//judges as Table of Contents if root element is toc
-$isToc = $xml->xpath('/toc');
-if(count($isToc) > 0){
-	echo 'file name: ',"\n", $file,"\n", '<BR>',"\n",
-		 'label attribute of toc: <br>',"\n", $isToc['0']['label'],"\n", '<br>',"\n",
-		 'label attributes of topics:<BR>',"\n";
-	//various levels exist for topic
-	$topics = array();
-	foreach ($xml->xpath('//topic') as $topic){
-		$topics[] = $topic['label'];
+/**
+ * @param string $col1
+ * @param string $col2
+ * @param string $col3
+ * @return void
+ */
+function printTableHeader($col1, $col2, $col3 = null) {
+	echo '<table border=1><tr>',
+		 '<th>', $col1, '</th>', "\n",
+		 '<th>', $col2, '</th>', "\n";
+	if($col3 !== null){
+	echo '<th>', $col3, '</th>';
 	}
-	sortAndList($topics);
-	return true;
+	echo '</tr>', "\n";
 }
 
-//judges as Context Help if contexts element is in root
-$isContextHelp = $xml->xpath('/contexts');
-if(count($isContextHelp) > 0){
-	echo 'file name: ',"\n", $file,"\n", '<BR>',"\n",
-		 'file type: Context Help',"\n",
-		 '<table border=1>',
-		 '<tr><th>id</th><th>desc.</th></tr>',"\n";
-	foreach ($xml->xpath('/contexts/context') as $context){
-		echo '<tr>',
-			 '<td>', $context['id'], '</td>',"\n",
-			 '<td>', $context->description, '</td>',
-			 '</tr>',"\n";
+/**
+ * @param string $col1
+ * @param string $col2
+ * @param string $col3
+ * @return void
+ */
+function printTableRow($col1, $col2, $col3 = null) {
+	echo '<tr>',
+		 '<td>', $col1, '</td>', "\n",
+		 '<td>', $col2, '</td>', "\n";
+	if($col3 !== null){
+	echo '<td>', $col3, '</td>';
 	}
+	echo '</tr>', "\n";
+}
+
+/**
+ * @return void
+ */
+function printTableEnd() {
 	echo '</table>',"\n";
-	echo 'label attributes of topics:<BR>',"\n";
-	$topics = array();
-	foreach ($xml->xpath('/contexts/context/topic') as $topic){
-		$topics[] = $topic['label'];
-	}
-	sortAndList($topics);
-	return true;
 }
 
-
-//judges as Simple Cheat Sheet if cheatsheet element is in root
-$isCheatsheet = $xml->xpath('/cheatsheet');
-if(count($isCheatsheet) > 0){
-
-	echo 'file name: ',"\n", $file,"\n", '<BR>',"\n",
-		 'file type: Cheat Sheet',"\n",
-		 '<table border=1>',
-		 '<tr><th>title</th><th>intro desc.</th></tr>',"\n";
-	foreach ($xml->xpath('/cheatsheet') as $cheatsheet){
-		$desc = (string)$cheatsheet->intro->description->b;
-		if (strlen($desc) == 0 ) {
-			$desc = (string)$cheatsheet->intro->description;
-		}
-		echo '<tr><td>', $cheatsheet['title'], '</td>',"\n",
-			 '<td>', $desc, '</td></tr>',"\n";
+/**
+ * @param array $labels
+ * @return void
+ */
+function printLabels($labels) {
+	if(count($labels) === 0){
+		return;
 	}
-	echo '</table>';
-
-	echo '<table border=1>',
-		 '<tr><th>title</th><th>desc.</th></tr>',"\n";
-	foreach ($xml->xpath('/cheatsheet/item') as $item){
-		$desc = $item->description;
-		echo '<tr>',
-			 '<td>', $item['title'], '</td>',"\n",
-			 '<td>', $desc, '</td>',"\n",
-			 '</tr>',"\n";
+	printTableHeader('topic/label', 'length');
+	foreach ($labels as $label) {
+		if(strpos($label, ' ') === false){
+			$label_for_print = 'label="' . $label . '"';
+		} else {
+			$label_for_print = $label;
+		}
+		printTableRow($label_for_print, strlen($label));
 	}
-	echo '</table>',"\n",
-		 '<hr>',"\n";
-	return true;
-}
-
-//judges as Composite Cheat Sheet if compositecheatsheet element is in root
-$isComposite = $xml->xpath('/compositeCheatsheet');
-if(count($isComposite) > 0){
-
-	echo 'file name: ',"\n", $file,"\n", '<BR>',"\n",
-		 'file type: Composite Cheat Sheet<BR>',"\n",
-		 'name attribute of sheet: <br>',"\n", $isComposite['0']['name'],"\n", '<BR>',"\n",
-		 '<table border=1>',
-		 '<tr><th>group name</th>',
-		 '<th>intro</th><th>compl.</th></tr>',"\n";
-	foreach ($xml->xpath('/compositeCheatsheet/taskGroup') as $taskGroup){
-		$intro = (string)$taskGroup->intro->b;
-		if (strlen($intro) == 0 ) {
-			$intro = (string)$taskGroup->intro;
-		}
-		$compl = (string)$taskGroup->onCompletion->b;
-		if (strlen($compl) == 0 ) {
-			$compl = (string)$taskGroup->onCompletion;
-		}
-		echo '<tr>',
-			 '<td>', $taskGroup['name'], '</td>',"\n",
-			 '<td>', $intro, '</td>',"\n",
-			 '<td>', $compl, '</td>',
-			 '</tr>',"\n";
-	}
-	echo '</table>',"\n";
-
-	echo '<table border=1>',
-		 '<tr><th>name</th>',
-		 '<th>intro</th><th>compl.</th></tr>',"\n";
-	foreach ($xml->xpath('/compositeCheatsheet/taskGroup/task') as $task){
-		$intro = (string)$task->intro->b;
-		if (strlen($intro) == 0 ) {
-			$intro = (string)$task->intro;
-		}
-		$compl = (string)$task->onCompletion->b;
-		if (strlen($compl) == 0 ) {
-			$compl = (string)$task->onCompletion;
-		}
-		echo '<tr>',
-			 '<td>', $task['name'], '</td>',"\n",
-			 '<td>', $intro, '</td>',"\n",
-			 '<td>', $compl, '</td>',"\n",
-			 '</tr>',"\n";
-	}
-	echo '</table>',"\n",
-		 '<hr>',"\n";
-	return true;
-}
-
-echo 'file name: ',"\n", $file,"\n", ' is <b>unknown</b> type.<BR>',"\n";
-return false;
+	printTableEnd();
 }
